b75275c866ff53db7a10acc49e9ccce815b4545b
Madhur82954 <itzmadhurverma@gmail.com>
2023-05-25 12:42:17 +0530

    [Madhur] - EmployeeAttendance Added

---
+++ EmployeeAttendance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageUsingOOPS
+{
+    internal class EmployeeAttendance
+    {
+        public void CheckEmployeeAttendance()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int empcheck = random.Next(0, 2);
+
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageUsingOOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeAttendance employee = new EmployeeAttendance();
+            employee.CheckEmployeeAttendance();
+        }
+    }
+}
